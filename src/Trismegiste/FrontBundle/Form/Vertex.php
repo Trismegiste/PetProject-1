0fb15810b0c71a96d6d8256db8acfd77154403c4
Trismegiste <flo@trismegiste.net>
2013-11-24 15:45:22 +0100

    editing form

--- src/Trismegiste/FrontBundle/Form/Vertex.php
+++ src/Trismegiste/FrontBundle/Form/Vertex.php
@@ -45,20 +45,22 @@ class Vertex extends AbstractType
                     'multiple' => false,
                     'constraints' => new Assert\NotBlank()
                 ])
-                ->add('title', 'text', ['constraints' => new Assert\NotBlank(), 'required' => true])
+                ->add('title', 'text', ['constraints' => [
+                        new Assert\NotBlank(),
+                        new Assert\Length(['min' => 3])
+                    ],
+                    'required' => true
+                ])
                 ->add('headline', 'text', ['required' => false, 'attr' => ['class' => 'span6']])
                 ->add('description', 'textarea', ['attr' => ['class' => 'span6 mentionable', 'rows' => 6]])
                 ->add('gmOnly', 'textarea', ['required' => false, 'attr' => ['class' => 'span6 mentionable', 'rows' => 6]])
                 ->add('save', 'submit')
-                ->add('delete', 'submit', ['attr' => ['class' => 'btn btn-danger']])
                 ->setDataMapper(new PropertyPathMapper(new PropertyAccessor(true)))
                 ->addEventListener(FormEvents::PRE_SET_DATA, function(FormEvent $event) {
                             $data = $event->getData();
                             $form = $event->getForm();
                             if (!is_null($data) && !is_null($data->getId())) {
                                 unset($form['infoType']);
-                            } else {
-                                unset($form['delete']);
                             }
                         });
     }
