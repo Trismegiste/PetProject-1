4c8e61a7ba5c390aede0b22d82e9b2168423baf8
Trismegiste <flo@trismegiste.net>
2013-12-11 15:25:29 +0100

    adding graphical view for graph

--- src/Trismegiste/FrontBundle/Controller/GraphController.php
+++ src/Trismegiste/FrontBundle/Controller/GraphController.php
@@ -8,6 +8,8 @@ namespace Trismegiste\FrontBundle\Controller;
 
 use Trismegiste\FrontBundle\Form\Graph;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Trismegiste\FrontBundle\Utils\Helper;
 
 /**
  * GraphController is a crud from graph model
@@ -58,6 +60,45 @@ class GraphController extends Template
         $this->pushFlash('notice', 'Working document is now : Graph ' . $graph->getTitle());
 
         return $this->redirectRouteOk('trismegiste_homepage');
+    }
+
+    public function showAction()
+    {
+        return $this->render('TrismegisteFrontBundle:Graph:show.html.twig');
+    }
+
+    public function getAllNodeAction()
+    {
+        $cursor = $this->getRepo()->findByGraph($this->getGraphFilter());
+        $nodes = [];
+        $links = [];
+
+        $shortcut = [];
+        $index = 0;
+        foreach ($cursor as $doc) {
+            $nodes[$index] = ['name' => $doc['title'], 'group' => 1];
+            $shortcut[$doc['slug']] = ['idx' => $index, 'edge' => $doc['description'] . ' ' . $doc['gmOnly']];
+            $index++;
+        }
+
+        foreach ($shortcut as $doc) {
+
+            $extract = [];
+            preg_match_all('#@([_\w]+)#', $doc['edge'], $extract);
+
+            foreach ($extract[1] as $mention) {
+                $edge = Helper::mentionToSlug($mention);
+                if (array_key_exists($edge, $shortcut)) {
+                    $links[] = [
+                        "source" => $doc['idx'],
+                        "target" => $shortcut[$edge]['idx'],
+                        "value" => 1
+                    ];
+                }
+            }
+        }
+
+        return new JsonResponse(['nodes' => $nodes, 'links' => $links]);
     }
 
 }
